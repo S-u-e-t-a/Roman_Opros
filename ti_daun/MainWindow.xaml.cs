4468d61ab814e4bb206b612e08ec04c82dc6644d
khlebnikovroman <khlebnikov.roman187@gmail.com>
2021-07-28 01:36:05 +0500

    Добавьте файлы проекта.

---
+++ ti_daun/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace ti_daun
+{
+    /// <summary>
+    ///     Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly Random rnd;
+        private bool canCloseWindow;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            rnd = new Random();
+            canCloseWindow = false;
+        }
+
+
+        private void UIElement_OnMouseMove(object sender, MouseEventArgs e)
+        {
+            var but = sender as Button;
+            var top = rnd.Next(0, Convert.ToInt32(Height - 50));
+            var left = rnd.Next(0, Convert.ToInt32(Width - but.Width));
+            but.Margin = new Thickness(left, top, 0, 0);
+            Trace.WriteLine($"{top},{left},{Height},{Width}");
+        }
+
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            YesButton.Visibility = Visibility.Hidden;
+            NoButton.Visibility = Visibility.Hidden;
+            Label.Content = "Я знал))))";
+            canCloseWindow = true;
+        }
+
+
+        private void MainWindow_OnClosing(object sender, CancelEventArgs e)
+        {
+            if (!canCloseWindow)
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
